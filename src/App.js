51b220ada56b5a8f8457822ba461476928875780
Nahuel61920 <estebancarrizo619@gmail.com>
2022-05-24 23:51:44 -0300

    Create Gifs App in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import AddCategory from './components/AddCategory'
+import DisplayGifs from './components/DisplayGifs';
 
 function App() {
+  const [category, setCategory] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Gifs App</h2>
+      <AddCategory setCategory={setCategory} />
+      <DisplayGifs category={category} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
